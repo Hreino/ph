f19e7f89913f0af4a7354852d872bb7c3dcb1586
Hector Reinosa <hreinosa@hotmail.com>
2018-12-14 09:14:19 -0600

    user edit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -79,7 +79,6 @@ class UserController extends Controller
     public function update(Request $request, $id)
     {
         $this->validate($request,[ 
-            'company' => ['required', 'string'],
             'role' => ['required', 'integer']
         ]);
 
